422770931d59425d5c3a36d91703afbf36ac647e
LumiNovryM <luminovrymekel@gmail.com>
2026-05-14 21:12:48 +0700

    feat: create register & notification popup

--- apps/frontend/web-app/obj/Debug/net8.0/web-app.AssemblyInfo.cs
+++ apps/frontend/web-app/obj/Debug/net8.0/web-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb17e8e8738224c368fbbee4b7b618869403ea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eab05fc3ec940bf852c2eca83efc8aabd51ce60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
